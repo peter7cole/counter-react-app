a9bb5e1c617ca01d0e06e980ee6b8093a441a91d
‘peter-robert-cole’ <petercole3@gmail.com>
2021-04-28 12:00:34 -0700

    Updates

---
+++ precache-manifest.cb556dc80225aa80e69a63db791c450e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160418241e2e959a461d7d0eb522d676",
+    "url": "/counter-react-app/index.html"
+  },
+  {
+    "revision": "3327f5bddfb8ab2e594d",
+    "url": "/counter-react-app/static/css/main.f0a4a4b4.chunk.css"
+  },
+  {
+    "revision": "b77c301148e349038d00",
+    "url": "/counter-react-app/static/js/2.add79468.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/counter-react-app/static/js/2.add79468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3327f5bddfb8ab2e594d",
+    "url": "/counter-react-app/static/js/main.2eef78a0.chunk.js"
+  },
+  {
+    "revision": "103819783063866578ca",
+    "url": "/counter-react-app/static/js/runtime-main.4f6c018d.js"
+  }
+]);
